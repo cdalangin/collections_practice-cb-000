0ca4093a83a9a493e84fc7be2c9742b18bafc76f
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-02 03:10:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def kesha_maker(array)
   array.map do |name|
     letters = name.split('')
     letters.map do |letter|
-      letter.insert("$").delete_at(3)
+      letter.insert(0, "$").delete_at(3)
     end
   end
 end
