89f909810bfcbb6a58320a9ea7631b6571a668c5
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-02 02:56:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-
 end
 
 def reverse_array(array)
@@ -30,4 +29,3 @@ def kesha_maker(array)
       letter[3] = $
     end
   end
-end
